feb7481fe8356a6598891eae93df188370ce1a7e
Miguel dos Santos <miguelmartinsdossantos@gmail.com>
2023-09-08 22:57:35 -0300

    :wrench: Configure dotenv

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,13 +1,11 @@
-import axios from "axios";
 import { LoginRequest, LoginResponse } from "../types/auth";
 import { User } from "../types/user";
-
-const API_BASE_URL = "http://localhost:5082/api/Auth";
+import axiosInstance from "./config";
 
 export const authService = {
   login: async (data: LoginRequest): Promise<LoginResponse> => {
     try {
-      const response = await axios.post<User>(`${API_BASE_URL}/login`, data);
+      const response = await axiosInstance.post<User>("/Auth/login", data);
       return {
         success: true,
         user: response.data,
